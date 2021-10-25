cb346e5b3aaf415915cbcb6857feae4cf4862415
bryantankink <tankinkbryan@gmail.com>
2021-10-25 16:09:14 +0200

    Adding toastr support

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {}
 
   ngOnInit(): void {
   }
@@ -23,14 +25,15 @@ export class NavComponent implements OnInit {
     body.append("password", this.model.password);
 
     this.accountService.login(body).subscribe(response => {
-      console.log(response);
-      //this.loggedIn = true;
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
   Logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
