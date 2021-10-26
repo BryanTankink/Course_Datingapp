b624709768c1b1ecba5aea87727d8e62a63f4553
bryantankink <tankinkbryan@gmail.com>
2021-10-26 15:09:07 +0200

    Basic data seeding

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int GetAge(this DateTime dateTime) {
+            var today = DateTime.Today;
+            var age = today.Year - dateTime.Year;
+            if(dateTime.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
